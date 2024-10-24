62ddea6cc69b5a3e370076f1cb893faf3d76ba20
ianvianini <ianvianini@gmail.com>
2024-10-24 14:37:25 -0300

    Alterada as descrições, adicionado alguns comentarios, referencia

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-// Classe Heroi com metodo construtor 
-class heroi{
+// Idéia é de implementar uma party de 4 pessoas como no jogo Valkyrie Profile / Project x Zone
+// Cada unidade possui um ataque que seria acionado quando o botão referente a unidade fosse pressionado
+// O desafio pede " idade " como  atributo da classe, embora eu nao acho uma funcionalidade importante 
+// Classe Unidade com metodo construtor 
+class unidade{
     constructor(nome, idade, tipo){
         this.nome = nome;
         this.idade = idade;
@@ -30,10 +33,10 @@ class heroi{
 }
 
 // Instânciando - instâncias (heh) da classe Heroi
-let unidade1 = new heroi("Samanosuke", 30, "Samurai");
-let unidade2 = new heroi("Patolino O Mago", 25, "Mago");
-let unidade3 = new heroi("Eddy Gordo", 32, "Capoeirista");
-let unidade4 = new heroi("Ryu", 40, "Andarilho");
+let unidade1 = new unidade("Samanosuke", 30, "Samurai");
+let unidade2 = new unidade("Patolino O Mago", 25, "Mago");
+let unidade3 = new unidade("Eddy Gordo", 32, "Capoeirista");
+let unidade4 = new unidade("Ryu", 40, "Andarilho");
 
 // Chamando o método Attack para cada herói
 unidade1.attack();
